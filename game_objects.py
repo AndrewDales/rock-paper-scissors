b9157eae609590b76841ca203d6dae4ba4c25ee7
Andrew Dales <andrew.dales@highgateschool.org.uk>
2022-11-23 21:59:42 +0000

    Added docstrings to game_objects.py

--- game_objects.py
+++ game_objects.py
@@ -22,11 +22,38 @@ RPS_WIN_DICT = {'rock': ['scissors'],
 
 # PlayerObject represents an object that a player could choose
 class PlayerObject:
+    """
+    A class to represent a playable object
+    ...
+    Attributes
+    ----------
+    name: str
+        name of the object
+    allowable_objects: tuple (class attribute)
+        list of allowable objects
+    win_dict: dict
+        keys are allowable objects, values is list of what keys will beat
+    Methods
+    -------
+    random_objects (class method)
+        returns a PlayerObject randomly chosen from the allowable objects
+    set_object_rules (class method)
+        sets the allowable objects and the win_dict for what the object can beat
+    """
+
     # Set default objects for the class
     allowable_objects = RPSLS_OBJECTS
     win_dict = RPSLS_WIN_DICT
 
     def __init__(self, name):
+        """
+        Constructs the attributes for the PlayerObject
+
+        Parameters
+        ----------
+            name: str
+                name of object - must be in allowable objects
+        """
         if name.lower() in self.allowable_objects:
             self.name = name.lower()
         else:
@@ -34,10 +61,16 @@ class PlayerObject:
 
     @classmethod
     def random_object(cls):
+        """
+        Returns a random object from amongst the allowable objects
+        """
         return PlayerObject(random.choice(cls.allowable_objects))
 
     @classmethod
     def set_object_rules(cls, allowable_objects=None, win_dict=None):
+        """
+        Sets the allowable objects and the win_dict for the class
+        """
         if allowable_objects:
             cls.allowable_objects = allowable_objects
         if win_dict:
@@ -47,18 +80,42 @@ class PlayerObject:
                 raise ValueError("Keys of win_dict must be the allowable objects")
 
     def __eq__(self, other):
+        """
+        Checks if two objects are the same
+        """
         return self.name == other.name
 
     def __gt__(self, other):
+        """
+        Checks if the current object (self) beats the passed object (other)
+        """
         return other.name in self.win_dict[self.name]
 
     def __repr__(self):
+        """
+        Representation of the object
+        """
         return f'PlayerObject({self.name})'
 
 
 # The Player Class represents a player
 class Player:
+    """
+    A class to represent a player of the game
+    """
     def __init__(self, name=None):
+        """
+        Constructs the necessary attributes for the Player class
+
+        Attributes
+        __________
+            name: str
+                Player name
+            score: int
+                Player score
+            current_object: PlayerObject or None
+                What the player's current object is None for not selected
+        """
         if name:
             self.name = name
         else:
@@ -67,44 +124,51 @@ class Player:
         self.current_object = None
 
     def set_name(self, name):
+        """ Sets name attribute to name """
         self.name = name
 
     def reset_object(self):
+        """ Sets the current_object to None - not selected"""
         self.current_object = None
 
     def win_round(self):
+        """ Increases score by one"""
         self.score += 1
 
     def __repr__(self):
+        """ Representation of the object """
         check_object_chosen = bool(self.current_object)
         return f'Player: {self.name}\nScore: {self.score}\nObject chosen: {check_object_chosen}'
 
 
 # The HumanPlayer Class is a subclass of Player representing a human player
 class HumanPlayer(Player):
+    """ Subclass of Player representing a human player (PC) """
     def choose_object(self, choice):
+        """ Chooses a PlayerObject for the player"""
         self.current_object = PlayerObject(choice)
 
 
 # The ComputerPlayer Class is a subclass of Player representing a Computer player
 class ComputerPlayer(Player):
+    """ Subclass of Player representing a Computer player (NPC) """
     def __init__(self):
+        """ Constructs super Player object with name "Computer """
         super().__init__('Computer')
 
     def choose_object(self):
+        """ Computer chooses a random PlayerObject """
         self.current_object = PlayerObject.random_object()
 
 
 # The Game class contains the instructions for running the game
 class Game:
-
+    """ """
     def __init__(self, allowable_objects=None, win_dict=None):
         if allowable_objects is None:
             allowable_objects = RPSLS_OBJECTS
         if win_dict is None:
             win_dict = RPSLS_WIN_DICT
-        # self.allowable_objects = allowable_objects
-        # self.win_dict = win_dict
         self.current_round = 0
         self.max_rounds = None
         self.players = []
